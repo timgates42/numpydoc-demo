0bda4a84038a15adfeaae273e789b00421041d25
Tim Gates <tim.gates@iress.com>
2019-05-30 08:41:22 +1000

    Add initial 3 muskateers dumb agent azure pipeline

---
+++ ci.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+if ! which make ; then
+    echo 'GNU Make is missing!' >&2
+    exit 1
+fi
+if ! which docker ; then
+    echo 'Docker is missing!' >&2
+    exit 1
+fi
+if ! which docker-compose ; then
+    echo 'Docker-Compose is missing!' >&2
+    exit 1
+fi
+true
